f5cc96aea29900add4e0eef1419ed6d877db523a
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2023-04-17 17:28:38 +0000

    new home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -10,12 +10,12 @@ class HomeActivities:
   #X Ray subsegment
   #def run(Logger):
     #Logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activities-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    # with tracer.start_as_current_span("home-activities-mock-data"):
+      # span = trace.get_current_span()
+      # now = datetime.now(timezone.utc).astimezone()
+      # span.set_attribute("app.now", now.isoformat())
       
-      sql = query_wrap_array("""
+      results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -30,16 +30,5 @@ class HomeActivities:
       FROM public.activities
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
-      """) 
-      print("SQL------------")
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      print("-----------")
-      print(json[0])
-      return json[0]
+      """)       
       return results
