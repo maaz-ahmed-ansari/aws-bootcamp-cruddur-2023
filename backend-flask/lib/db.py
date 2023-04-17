e38752226b35993e5334ea178559d9dba2688753
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2023-04-17 18:22:47 +0000

    refactor db library

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -25,7 +25,7 @@ class Db:
     print("SQL STATEMENT - [array]------------")
     print(sql)
     wrapped_sql = self.query_wrap_array(sql)
-    with selfpool.connection() as conn:
+    with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(wrapped_sql)
         json = cur.fetchone()
